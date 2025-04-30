65cc7ba9ffb231302a0fbd3e2b0ff557540af8d4
Welena <welena@list.ru>
2025-04-30 11:35:39 +0700

    step 4 styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 
 
 function App() {
